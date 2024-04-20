c507726cc80471ec6b8a75a8899e92aa61948f25
Marshal Horn <kamocat@gmail.com>
2024-04-20 08:48:42 -0700

    Too many issues. Moving to a simpler design

--- sim/boost.py
+++ sim/boost.py
@@ -4,8 +4,8 @@ from math import sqrt
 Li = 0
 Lh = 47e-6
 Cout = 100e-6
-Vin = 2
-Vout = 0
+Vin = 3
+Vout = Vin + 1
 Iload = 0
 
 data = []
@@ -20,24 +20,24 @@ class Regulator:
         self.vout = 0
 
     def update(self, Vin, Vout, Iload):
-        self.Li = (Vout - self.vout) * Cout / self.toff
+        self.Li = (Vout - self.vout) * Cout / self.toff + Iload 
         self.vout = Vout
         vdif = self.vset - self.vout
         if vdif > 0:
             coulombs_needed = vdif * Cout
             amps_needed = sqrt(coulombs_needed / Lh * (vdif/2+Vin))
-            amps_needed += Iload - self.Li
+            amps_needed -= self.Li
         else:
-            amps_needed = Iload - self.Li
-        self.duty = amps_needed / (self.period * Vin / Lh)
-        if self.duty>0.8:
-            duty = 0.8
-        elif self.duty < 0:
-            duty = 0
+            amps_needed = Iload
+        if Vout > Vin:
+            self.duty = amps_needed * Lh * (1-Vin/Vout) / self.period
+            max_duty = 1 - 0.5*Vin/Vout
+            if self.duty>max_duty:
+                self.duty = max_duty
         else:
-            duty = self.duty
-        self.ton = self.period * duty
-        self.toff = self.period * (1-duty)
+            self.duty = 0
+        self.ton = self.period * self.duty
+        self.toff = self.period * (1-self.duty)
         return self.ton, self.toff
     def setpoint(self, setpoint):
         self.vset = setpoint
@@ -66,14 +66,14 @@ class Charger():
                 if Vout > 6 or Vout < 4:
                     self.speed = 0
                 elif self.amps > 0.8 and Vout >= 5:
-                    self.speed = 3
+                    self.speed = 2 #Disable fast-charging for now
             case 3: # Fast charging
                 if Vout > 9 or Vout < 4.8:
                     self.speed = 1
         if self.speed > 0:
             self.amps = self.watts[self.speed] / Vout
         else:
-            self.amps = .1
+            self.amps = 0.1
         return self.amps
 
 u1 = Regulator(5.0)
@@ -89,9 +89,10 @@ for j in range(int(5e3)):
     Vout += Toff * (Li - di/2) / Cout
     Vout -= (Ton + Toff) * Iload / Cout
     Li -= di
-    data.append((Vout,Iload,u2.speed))
+    data.append((Vout,u1.duty,Iload,Li,u1.Li,u2.speed))
 
 for d in zip(*data):
     plt.plot(list(d))
+plt.legend(['Vout','Duty','Iload','Li','u1.Li','speed'])
 plt.show()
 
